ee43bc5010698a3c4ffc24d3710b3ef1a05b5122
Jérémy WALTHER <jeremy.walther@virtua.ch>
2018-01-30 15:31:46 +0100

    Remove test function

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,3 @@
-Parse.Cloud.define("hello", function(request, response) {
-    console.log("TEST LOG");
-    console.warn("TEST WARN");
-    console.error("TEST ERROR");
-    response.success("OK");
-});
-
 console.info('Parse Cloud Code is initializing ...');
 const vpx = require('./virtua-parse-extended');
 vpx.init();
